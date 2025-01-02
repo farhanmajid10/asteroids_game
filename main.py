663ae81d8cebb1aad9b06cc4e4f8b3af6b9d4852
farhanmajid10 <farhanmajid23@gmail.com>
2025-01-02 01:16:32 -0600

    Implemented Rotation.

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 import pygame
-
+from player import *
 from constants import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    running = True
+    dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x,y)
+    #running = True
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = clock.tick(60)/1000
         screen.fill("black")
+        player.update(dt)
+        #player.rotate(dt)
+        player.draw(screen)
         pygame.display.flip()
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
